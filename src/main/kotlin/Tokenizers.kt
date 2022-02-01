8d75c26e2d8ba2fb6989475744ce8501c29c5d0b
gsycl2004 <1544189298@qq.com>
2022-02-01 22:45:28 +0800

    增加了通过正则表达式进行过滤的功能

--- src/main/kotlin/Tokenizers.kt
+++ src/main/kotlin/Tokenizers.kt
@@ -3,7 +3,7 @@ package com.github
 import com.kennycason.kumo.nlp.tokenizer.api.WordTokenizer
 import com.kennycason.kumo.nlp.tokenizers.ChineseWordTokenizer
 
-enum class Tokenizer(val instance: WordTokenizer){
+enum class Tokenizers(val instance: WordTokenizer){
     JIEBA(JieBaTokenizer()),
     KUMO(ChineseWordTokenizer())
 }
